57c136e076d8772f558abeafb2ead7277f381b86
crener <12804622+crener@users.noreply.github.com>
2021-12-28 15:42:44 +0000

    Add project files.

---
+++ ECS Benchmark/Benchmarks/CreateBenchmarks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using DefaultEcs;
+using ECS_Benchmark.Frameworks;
+
+namespace ECS_Benchmark
+{
+    [MemoryDiagnoser()]
+    public class CreateBenchmarks
+    {
+        [Params(100, 1000, 5000, 10000, 20000, 100000, 1000000, 5000000, 10000000, 15000000, 20000000)]
+        public int EntityQuantity { get; set; }
+
+
+        [Benchmark]
+        public void DefaultEcs()
+        {
+            DefaultEcsBenchmark.CreateEntities(EntityQuantity);
+        }
+
+        [Benchmark]
+        public void LeoEcs()
+        {
+            DefaultEcsBenchmark.CreateEntities(EntityQuantity);
+        }
+    }
+}
---
+++ ECS Benchmark/Frameworks/LeoECS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_Benchmark.Frameworks
+{
+    public static class LeoECSBenchmarks
+    {
+        public static void CreateEntities(int count)
+        {
+
+        }
+    }
+}
